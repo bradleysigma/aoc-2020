d275b2e292363c05d07f4019456dd634a2b57c9d
bradleysigma <42644678+bradleysigma@users.noreply.github.com>
2020-12-04 00:35:28 +1000

    Day 3

---
+++ day3.py
@@ -0,0 +1,20 @@
+import aoc
+data = aoc.strlist(3)
+
+def path(geo, u, v):
+    n = 0
+    j = 0
+    d = len(geo[0])
+    for i in geo[::v]:
+        if i[j%d] == "#":
+            n += 1
+        j += u
+    return n
+
+n = path(data, 3, 1)
+print(n)
+for x, y in [(1,1), (5,1), (7,1), (1,2)]:
+    n *= path(data, x, y)
+print(n)
+
+aoc.tock()
