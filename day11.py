58842e79dbf7e32a9e9a56a6c3ab9cfcc0e3cfac
bradleysigma <42644678+bradleysigma@users.noreply.github.com>
2020-12-11 19:14:33 +1000

    Updates
    20sec => 2.5sec

--- day11.py
+++ day11.py
@@ -1,51 +1,65 @@
 import aoc
-from collections import defaultdict
 data = aoc.strlist(11)
 
 h = len(data)
 w = len(data[0])
-p = defaultdict(lambda: ".")
-q = defaultdict(lambda: ".")
+p = {}
+q = {}
 for i in range(w):
     for j in range(h):
+        if data[j][i] == ".": continue
         p[(i,j)] = data[j][i]
         q[(i,j)] = data[j][i]
 
-while True:
-    pp = defaultdict(lambda: ".")
-    for i in range(w):
-        for j in range(h):
-            n = [p[(i+u,j+v)] for u,v in ((1,1), (1,0), (1,-1), (0,-1), (-1,-1), (-1,0), (-1,1), (0,1))].count("#")
-            if p[(i,j)] == "L" and n == 0:
-                pp[(i,j)] = "#"
-            elif p[(i,j)] == "#" and n >= 4:
-                pp[(i,j)] = "L"
-            else:
-                pp[(i,j)] = p[(i,j)]
-    if all(p[k] == pp[k] for k in p.keys()): break
-    p = pp
+d = {}
+for i in range(w):
+    for j in range(h):
+        d[(i,j)] = []
+        for u,v in ((1,1), (1,0), (1,-1), (0,-1), (-1,-1), (-1,0), (-1,1), (0,1)):
+            if (i+u,j+v) in p:
+                d[(i,j)].append((i+u,j+v))
 
-while True:
-    qq = defaultdict(lambda: ".")
-    for i in range(w):
-        for j in range(h):
-            if q[(i,j)] == ".": continue
-            n = 0
-            for u,v in ((1,1), (1,0), (1,-1), (0,-1), (-1,-1), (-1,0), (-1,1), (0,1)):
-                k = 1
-                while (0 <= i+k*u < w) and (0 <= j+k*v < h):
-                    if q[(i+k*u,j+k*v)] != ".":
-                        n += int(q[(i+k*u,j+k*v)] == "#")
-                        break
-                    k += 1
-            if q[(i,j)] == "L" and n == 0:
-                qq[(i,j)] = "#"
-            elif q[(i,j)] == "#" and n >= 5:
-                qq[(i,j)] = "L"
-            else:
-                qq[(i,j)] = q[(i,j)]
-    if all(q[k] == qq[k] for k in q.keys()): break
+c = set(p.keys())
+while c:
+    pp = dict(p)
+    cc = set()
+    for i,j in c:
+        n = [p[k] for k in d[(i,j)]].count("#")
+        if p[(i,j)] == "L" and n == 0:
+            pp[(i,j)] = "#"
+            cc.update(d[(i,j)])
+        elif p[(i,j)] == "#" and n >= 4:
+            pp[(i,j)] = "L"
+            cc.update(d[(i,j)])
+    p = pp
+    c = cc
+
+d = {}
+for i in range(w):
+    for j in range(h):
+        d[(i,j)] = []
+        for u,v in ((1,1), (1,0), (1,-1), (0,-1), (-1,-1), (-1,0), (-1,1), (0,1)):
+            r = 1
+            while (0 <= i+r*u < w) and (0 <= j+r*v < h):
+                if (i+r*u,j+r*v) in q:
+                    d[(i,j)].append((i+r*u,j+r*v))
+                    break
+                r += 1
+
+c = set(q.keys())
+while c:
+    qq = dict(q)
+    cc = set()
+    for i,j in c:
+        n = [q[k] for k in d[(i,j)]].count("#")
+        if q[(i,j)] == "L" and n == 0:
+            qq[(i,j)] = "#"
+            cc.update(d[(i,j)])
+        elif q[(i,j)] == "#" and n >= 5:
+            qq[(i,j)] = "L"
+            cc.update(d[(i,j)])
     q = qq
+    c = cc
 
 print(list(p.values()).count("#"), list(q.values()).count("#"))
 
